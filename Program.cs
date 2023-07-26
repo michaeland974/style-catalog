78d6bf4e32dce592f09a6bf6ffbf8d1deb07387e
michaeland974 <micanderson29@gmail.com>
2023-07-26 11:46:47 -0500

    refactor: removed class to fix compile error with aspnet-codegenerator

--- Program.cs
+++ Program.cs
@@ -1,35 +1,35 @@
-public class Program
-{
-    public static void Main(string[] args){
-        string root = Directory.GetCurrentDirectory();
-        string dotenv = Path.Combine(root, "enviornment.env");
-        DotEnv.Load(dotenv);
+using style_catalog.Data;
+using Microsoft.EntityFrameworkCore;
 
-        Build(args);
-    }
+string root = Directory.GetCurrentDirectory();
+string dotenv = Path.Combine(root, "enviornment.env");
+DotEnv.Load(dotenv);
 
-    public static void Build(string[] args){
-        var builder = WebApplication.CreateBuilder(args);
-        builder.Services.AddControllersWithViews();
-      
-        var app = builder.Build();
+var builder = WebApplication.CreateBuilder(args);
+var connection = Environment.GetEnvironmentVariable("POSTGRESQL__CONNECTIONSTRING");
 
-        // Configure the HTTP request pipeline.
-        if (!app.Environment.IsDevelopment()){
-            app.UseExceptionHandler("/Home/Error");
-            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-            app.UseHsts();
-        }
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString(connection) ?? 
+        throw new InvalidOperationException("Connection string not found.")));
 
-        app.UseHttpsRedirection();
-        app.UseStaticFiles();
-        app.UseRouting();
-        app.UseAuthorization();
+var app = builder.Build();
 
-        app.MapControllerRoute(
-            name: "default",
-            pattern: "{controller=Home}/{action=Index}/{id?}");
-
-        app.Run();
-    }
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment()){
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
+
