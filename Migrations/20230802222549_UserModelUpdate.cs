fbb911f2ecedbb6161cff8b170dbc689ad79b9e3
michaeland974 <micanderson29@gmail.com>
2023-08-02 17:26:53 -0500

    fix: User Model updated

---
+++ Migrations/20230802222549_UserModelUpdate.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace style_catalog.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserModelUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Role",
+                table: "User",
+                newName: "role");
+
+            migrationBuilder.RenameColumn(
+                name: "PasswordHash",
+                table: "User",
+                newName: "passwordHash");
+
+            migrationBuilder.RenameColumn(
+                name: "FirstName",
+                table: "User",
+                newName: "firstName");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "User",
+                newName: "id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "role",
+                table: "User",
+                newName: "Role");
+
+            migrationBuilder.RenameColumn(
+                name: "passwordHash",
+                table: "User",
+                newName: "PasswordHash");
+
+            migrationBuilder.RenameColumn(
+                name: "firstName",
+                table: "User",
+                newName: "FirstName");
+
+            migrationBuilder.RenameColumn(
+                name: "id",
+                table: "User",
+                newName: "Id");
+        }
+    }
+}
