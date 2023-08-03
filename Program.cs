0a5d9a42df256c7132fe2ec31af1fb1b41bf2f48
michaeland974 <micanderson29@gmail.com>
2023-08-03 11:53:20 -0500

    redirect added for default route

--- Program.cs
+++ Program.cs
@@ -23,8 +23,9 @@ app.UseStaticFiles();
 app.UseRouting();
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapControllers();
+app.MapGet("/", (context) => {
+    return Task.Run(() => context.Response.Redirect("/Home"));
+});
 
 app.Run();
