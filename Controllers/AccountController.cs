9a3e0b3561d694c48ab46d5137510587bdced119
michaeland974 <micanderson29@gmail.com>
2023-09-04 22:24:37 -0500

    feat: Account Controller updated for register

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,121 +1,39 @@
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
+#nullable disable
+
 using style_catalog.Models;
-using style_catalog.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
-namespace style_catalog.Controllers
-{
-  [Route("Account/[action]")]
-  public class AccountController : Controller{
-    private readonly AccountContext _context;
-    public AccountController(AccountContext context){
-        _context = context;
+namespace style_catalog.Controllers;
+
+[Route("Account/[action]")]
+public class AccountController : Controller{
+    private readonly UserManager<Account> _userManager;
+
+    [HttpGet]
+    public IActionResult Register(){
+      return View();
     }
 
-    private bool AccountExists(int id){
-        return (_context.Account?.Any(e => e.id == id)).GetValueOrDefault();
-    }
-
-    // GET: Account
-    public async Task<IActionResult> Index(){
-        return _context.Account != null ? 
-            View(await _context.Account.ToListAsync()) :
-            Problem("Entity set 'AccountContext.Account'  is null.");
-    }
-
-    // GET: Account/Details/5
-    public async Task<IActionResult> Details(int? id){
-        if (id == null || _context.Account == null){
-            return NotFound();
-        }
-        var Account = await _context.Account.FirstOrDefaultAsync(m => m.id == id);
-        if (Account == null){
-            return NotFound();
-        }
-        return View(Account);
-    }
-
-    // GET: Account/Create
-    public IActionResult Create(){
-        return View();
-    }
-
-    // POST: Account/Create
     [HttpPost]
     [ValidateAntiForgeryToken]
-    public async Task<IActionResult> Create([Bind("Id,FirstName,Accountname,Role,PasswordHash")] Account Account){
-        if (ModelState.IsValid){
-            _context.Add(Account);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
+    public async Task<IActionResult> Register(Account accountModel){
+      if(!ModelState.IsValid){
+        return View(accountModel);
+      }
+      
+      var userAccount = new Account();
+      var result = await _userManager
+        .CreateAsync(userAccount, accountModel.password);
+      
+      if(!result.Succeeded){
+        foreach (var error in result.Errors){
+            ModelState.TryAddModelError(error.Code, error.Description);
         }
-        return View(Account);
-    }
+        return View(accountModel);
+      }
+      await _userManager.AddToRoleAsync(userAccount, "Visitor");
 
-    // GET: Account/Edit/5
-    public async Task<IActionResult> Edit(int? id){
-        if (id == null || _context.Account == null){
-            return NotFound();
-        }
-        var Account = await _context.Account.FindAsync(id);
-        if (Account == null){
-            return NotFound();
-        }
-        return View(Account);
+      return RedirectToAction(nameof(HomeController.Home), "Home");
     }
-
-    // POST: Account/Edit/5
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    public async Task<IActionResult> Edit(int id, [Bind("Id,FirstName,Accountname,Role,PasswordHash")] Account Account){
-        if (id != Account.id){
-            return NotFound();
-        }
-
-        if (ModelState.IsValid){
-            try{
-                _context.Update(Account);
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException){
-                if (!AccountExists(Account.id)){
-                    return NotFound();
-                }
-                else{
-                    throw;
-                }
-            }
-            return RedirectToAction(nameof(Index));
-        }
-        return View(Account);
-    }
-
-    // GET: Account/Delete/5
-    public async Task<IActionResult> Delete(int? id){
-        if (id == null || _context.Account == null){
-            return NotFound();
-        }
-        var Account = await _context.Account.FirstOrDefaultAsync(m => m.id == id);
-        if (Account == null){
-            return NotFound();
-        }
-        return View(Account);
-    }
-
-    // POST: Account/Delete/5
-    [HttpPost, ActionName("Delete")]
-    [ValidateAntiForgeryToken]
-    public async Task<IActionResult> DeleteConfirmed(int id){
-        if (_context.Account == null){
-            return Problem("Entity set 'AccountContext.Account' is null.");
-        }
-        var Account = await _context.Account.FindAsync(id);
-        if (Account != null){
-             _context.Account.Remove(Account);
-        }
-        
-        await _context.SaveChangesAsync();
-        return RedirectToAction(nameof(Index));
-    }
-  }
 }
