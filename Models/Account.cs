30b6275d1259453dee5de44b7f16ea1e30ffe9fa
michaeland974 <micanderson29@gmail.com>
2023-09-05 20:25:12 -0500

    Account model updated with attributes

--- Models/Account.cs
+++ Models/Account.cs
@@ -1,12 +1,19 @@
-using System.Text.Json.Serialization;
+using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Identity;
 
 namespace style_catalog.Models;
 
 public class Account : IdentityUser{
+  [Key]
   public int id { get; set; }
   public string? firstName { get; set; }
   public string? username { get; set; }
-  [JsonIgnore]
-  public string? passwordHash { get; set; }
+
+  [Required(ErrorMessage = "Password is required")]
+  [DataType(DataType.Password)]
+  public string? password { get; set; }
+
+  [DataType(DataType.Password)]
+  [Compare("password", ErrorMessage = "The password and confirmation password do not match.")]
+  public string? confirmPassword { get; set; }
 }
