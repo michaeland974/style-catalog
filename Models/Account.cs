6fa576869f646bcb286cb69257458b67a229ccc0
michaeland974 <micanderson29@gmail.com>
2023-09-28 21:25:28 -0500

    refactor: Model attribute updated for readability

--- Models/Account.cs
+++ Models/Account.cs
@@ -1,18 +1,20 @@
 using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Identity;
 
+#nullable disable
+
 namespace style_catalog.Models;
 
 public class Account : IdentityUser{
   [Key]
-  public int Id { get; set; }
-  public string UserName { get; set; }
+  public override string Id { get; set; }
+  public override string UserName { get; set; } 
 
   [Required(ErrorMessage = "Password is required")]
   [DataType(DataType.Password)]
   public string Password { get; set; }
 
   [DataType(DataType.Password)]
-  [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+  [Compare(nameof(Password), ErrorMessage = "The password and confirmation password do not match.")]
   public string ConfirmPassword { get; set; }
 }
